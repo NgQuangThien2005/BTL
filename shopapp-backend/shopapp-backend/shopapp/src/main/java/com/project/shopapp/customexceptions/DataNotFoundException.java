3793391288bb7272a3b24da3f54be77d77eba1af
Thiennguyen113 <nguyenquangthien1132005@gmail.com>
2025-06-06 23:40:14 +0700

    shopapp

--- shopapp-backend/shopapp-backend/shopapp/src/main/java/com/project/shopapp/customexceptions/DataNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.project.shopapp.customexceptions;
-
-public class DataNotFoundException extends RuntimeException{
-    public DataNotFoundException(String message) {
-        super(message);
-    }
-}
